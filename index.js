b48b6a2abb81515bd5c30a70c03228c6a9e9d64e
Mark Wylde <me@markwylde.com>
2020-10-27 18:27:50 +1000

    Add exit links

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const chalk = require('chalk');
 chalk.level = 3;
 
 const server = http.createServer((request, response) => {
+  if (request.url === '/quit/0') {
+    console.log('User requested exit (code 0)');
+    process.exit(0);
+  }
+
+  if (request.url === '/quit/1') {
+    console.log('User requested exit (code 1)');
+    process.exit(1);
+  }
+
   const hostname = os.hostname();
   console.log(`Incoming request from "${chalk.yellow(request.connection.remoteAddress)}" to "${chalk.yellow(`${request.method}:${request.url}`)}"`);
   response.writeHead(200, { 'content-type': 'text/html' });
@@ -12,10 +22,17 @@ const server = http.createServer((request, response) => {
     <html>
       <head>
         <title>Hello World</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
       </head>
 
       <body>
-        Hello from <strong>${hostname}</strong> at <strong>${new Date()}</strong>.
+        <p>Hello from <strong>${hostname}</strong> at <strong>${new Date()}</strong>.</p>
+        <p>
+          <ul>
+            <li><a href="/quit/0">Quit Node App (Code 0)</a></li>
+            <li><a href="/quit/1">Quit Node App (Code 1)</a></li>
+          </ul>
+        </p>
       </body>
     </html>
   `);
@@ -27,4 +44,3 @@ server.on('listening', () => {
 });
 
 server.listen(process.env.WEB_PORT || 8000);
-
